212a984e353f152699ef873f4e00d8f20cb0e9b6
Branislav Radomirov <bane-93erep@hotmail.com>
2025-11-21 10:21:18 +0100

    Add RSpec, RuboCop, and GitHub Actions CI
    Also added dummy Rails app backed by
    PostgreSQL, and spec_helper with sane defaults to test gem.

--- lib/watchtower/engine.rb
+++ lib/watchtower/engine.rb
@@ -2,14 +2,14 @@
 
 module Watchtower
   class Engine < ::Rails::Engine
-    engine_name "watchtower"
+    engine_name 'watchtower'
 
     isolate_namespace Watchtower
 
-    initializer "watchtower.append_migrations" do |app|
+    initializer 'watchtower.append_migrations' do |app|
       unless app.root.to_s == root.to_s
-        app.config.paths["db/migrate"].concat(
-          config.paths["db/migrate"].expanded
+        app.config.paths['db/migrate'].concat(
+          config.paths['db/migrate'].expanded
         )
       end
     end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('dummy/config/environment', __dir__)
+
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
